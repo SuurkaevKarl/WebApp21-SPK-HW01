1ad8275bfc666d0eee846cf6ec1acee1ba024a5d
SuurkaevKarl <suurkarl@gmail.com>
2021-10-21 15:17:37 +0300

    Changed script to better fit existing post form + post image width set in .css

--- res/js/script.js
+++ res/js/script.js
@@ -10,22 +10,25 @@ $(function() {
         for (post of posts) {
 
             let div = $('<div class= "content-container">');
-            let head = $('<h1>').text(post.caption);
-
+            let profilePic = $('<img class="prof-icon">').attr("src", "res/images/profile_icon_blank.png");
+            let timestamp = $('<span class="timestamp">').text(post.timestamp);
+            let caption = $('<span class="caption">').text(post.caption);
             let a = $('<a href="https://i.imgur.com">');
 
-            let picture = $('<img class="content-img">').attr("src", post.content).attr("width", "150").attr("height", "70");
+            let picture = $('<img class="content-img">').attr("src", post.content);//.attr("width", "150").attr("height", "70");
 
             //let a = $('<a href="https://icatcare.org">');
 
             //let picture = $('<img class="content-img">').attr("src", "https://icatcare.org/app/uploads/2018/07/Helping-your-new-cat-or-kitten-settle-in-1.png").attr("width", "150").attr("height", "70");//post.content);
 
-            
+            div.append(profilePic);
+            div.append(timestamp);
+
             a.append(picture);
-
-            div.append(head);
-
             div.append(a);
+
+            div.append(caption);
+
             section.append(div);
             
         }
