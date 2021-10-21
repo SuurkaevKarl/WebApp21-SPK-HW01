740406bab2e3350c1da9accd0afff5e05d63eab4
vpaabo <villempaabo@gmail.com>
2021-10-21 15:01:43 +0300

    added getting picture links from a server-hosted json file

--- res/js/script.js
+++ res/js/script.js
@@ -3,3 +3,32 @@
 function redirect() {
     window.location.href = "index.html";
 }
+
+$(function() {
+     $.get("https://api.jsonbin.io/b/617154ed9548541c29c6664c", function(posts) {
+         let section = $('<section>');
+        for (post of posts) {
+
+            let div = $('<div class= "content-container">');
+            let head = $('<h1>').text(post.caption);
+
+            let a = $('<a href="https://i.imgur.com">');
+
+            let picture = $('<img class="content-img">').attr("src", post.content).attr("width", "150").attr("height", "70");
+
+            //let a = $('<a href="https://icatcare.org">');
+
+            //let picture = $('<img class="content-img">').attr("src", "https://icatcare.org/app/uploads/2018/07/Helping-your-new-cat-or-kitten-settle-in-1.png").attr("width", "150").attr("height", "70");//post.content);
+
+            
+            a.append(picture);
+
+            div.append(head);
+
+            div.append(a);
+            section.append(div);
+            
+        }
+        $('body').append(section);
+    })
+}); 
